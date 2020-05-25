0c237aba0bb33b17b4d57864ef0ff4a4bc6bad6f
Shawn Alexander <shawn.k.alexander@gmail.com>
2020-05-25 21:22:59 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,7 +1,22 @@
-# require modules here
+require 'pry'
+require "yaml"
+library_hash = {}
 
-def load_library
-  # code goes here
+def load_library(path)
+  emoticons = YAML.load_file(path)
+  emoticon_translate = Hash.new
+
+  emoticons.each do |english_word, emoticon_set|
+    
+   
+  emoticon_translate.push = {english_word => {:english => emoticon_set.first, :japanese => emoticon_set.last}} 
+  
+    end
+    binding.pry
+emoticon_translate
+  
+   
+   
 end
 
 def get_japanese_emoticon
